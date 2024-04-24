9020da411965e817d80990bd5d051acdd34d1d57
l3marr <l3marquez@ucsd.edu>
2024-04-24 11:27:30 -0700

    commit

--- ChoreCharm/App.js
+++ ChoreCharm/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button, Image, TextInput } from "react-native";
 import { LoginScreen } from "./login.js";
 import { CreateChore } from "./CreateChore.js";
 import { AddRoom } from "./AddRoom.js";
@@ -22,11 +22,41 @@ function App() {
 
 export default App;
 
+const Login = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+    <View style ={styles.button}>
+    <Button
+      title="Go to Create Chore Space"
+      onPress={() => navigation.navigate('CreateChoreSpace', {name: 'CreatChoreSpace'}) }
+      color="#000000"
+    />
+     </View>
+     </View>
+  ) ;
+};
+const Create = ({navigation, route}) => {
+  return <Text>This is {route.params.name}'s profile</Text>;
+};
+
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    padding: 1,
+  },
+
+  button: {
+    flex: 2,
+    justifyContent: "center",
+    backgroundColor: "#FCF6E5",
+    height: "20%",
+    width: "70%",
+    margin: "2%",
+    borderRadius: 10,
   },
 });
